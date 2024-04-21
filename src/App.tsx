82fb64031bd8a002d424847a013accef6ac7df05
OfiOli <subelzaolivitocabezas@gmail.com>
2024-04-21 08:34:35 -0400

    base

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         />
       </div>
       <h1 className="text-4xl font-light animate-pulse text-white mt-8 cursor-pointer">
-        oliviodev.com is being built!
+        Oliviodev.com is being built! 💻
       </h1>
     </div>
   );
