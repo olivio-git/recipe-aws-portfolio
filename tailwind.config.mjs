c39dc797475b6bd4a262c86ddc8f1651078e586c
OfiOli <subelzaolivitocabezas@gmail.com>
2024-04-23 01:57:52 -0400

    features:darkmode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode:"class",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+			  // Definir colores específicos para el modo oscuro si es necesario
+			  dark: {
+				primary: '#212121',  
+			  },
+			  light:{
+				primary: '#e8e8e8',
+				orangeprimary:"#d9a7c7",
+				orangesecondary:"#fffcdc"
+			  }
+			},
+		}, 
 	},
 	plugins: [],
 }
